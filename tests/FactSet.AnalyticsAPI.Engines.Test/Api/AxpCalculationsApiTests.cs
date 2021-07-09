af1ad8198342623b5bb41761c8f3763384210b9a
Hank <hank.lin@factset.com>
2021-07-09 10:46:36 -0400

    added axp optimizer test

---
+++ tests/FactSet.AnalyticsAPI.Engines.Test/Api/AxpCalculationsApiTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using FactSet.AnalyticsAPI.Engines.Api;
+using FactSet.AnalyticsAPI.Engines.Model;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FactSet.AnalyticsAPI.Engines.Test.Api
+{
+    [TestClass]
+    public class AxpCalculationsApiTests
+    {
+        private AXPOptimizerApi axpOptimizerApi;
+
+        [TestInitialize]
+        public void Init()
+        {
+            axpOptimizerApi = new AXPOptimizerApi(CommonFunctions.BuildConfiguration());
+        }
+
+        [TestMethod]
+        public void OptimizerWorkflow_Should_Succeed_When_GivenValidInputs()
+        {
+            var strategyId = "Client:/Optimizer/TAXTEST";
+            var accountId = "BENCH:SP50";
+            var riskModelDate =   "09/01/2020";
+            var backtestDate = "09/01/2020";
+            var axpStrategy = new AxiomaEquityOptimizerStrategy(null, strategyId);
+            var axpAccount = new OptimizerAccount(accountId);
+            var optimization = new Optimization(riskModelDate, backtestDate);
+            var tradesList = new OptimizerTradesList(OptimizerTradesList.IdentifierTypeEnum.SedolChk, false);
+            var outputTypes = new OptimizerOutputTypes(tradesList);
+
+            var axpCalculationParameters = new AxiomaEquityOptimizationParameters(axpStrategy, axpAccount, optimization, outputTypes);
+            var axpCalculationParameterRoot = new AxiomaEquityOptimizationParametersRoot(axpCalculationParameters);
+
+            var postAndOptimizeHttpResponse = axpOptimizerApi.PostAndOptimizeWithHttpInfo(null, null, axpCalculationParameterRoot);
+            //var postAndOptimizeHttpResponse = axpOptimizerApi.PostAndOptimizeWithHttpInfo(axiomaEquityOptimizationParametersRoot : axpCalculationParameterRoot);
+
+            postAndOptimizeHttpResponse.StatusCode
+                .Should()
+                .Match<HttpStatusCode>(c => c == HttpStatusCode.Created || c == HttpStatusCode.Accepted);
+
+            switch (postAndOptimizeHttpResponse.StatusCode)
+            {
+                case HttpStatusCode.Accepted:
+                    var optimizationId = postAndOptimizeHttpResponse.Headers["X-Factset-Api-Calculation-Id"].First();
+                    bool shouldPoll;
+                    do
+                    {
+                        var pollingResponse = axpOptimizerApi.GetOptimizationStatusByIdWithHttpInfo(optimizationId);
+                        pollingResponse.StatusCode
+                            .Should()
+                            .Match<HttpStatusCode>(c => c == HttpStatusCode.Created || c == HttpStatusCode.Accepted);
+
+                        shouldPoll = pollingResponse.StatusCode == HttpStatusCode.Accepted;
+
+                        if (pollingResponse.Headers.ContainsKey("Cache-Control"))
+                        {
+                            var maxAge = pollingResponse.Headers["Cache-Control"][0];
+                            if (string.IsNullOrWhiteSpace(maxAge))
+                            {
+                                Console.WriteLine("Sleeping for 2 seconds");
+                                // Sleep for at least 2 seconds.
+                                Thread.Sleep(2000);
+                            }
+                            else
+                            {
+                                var age = int.Parse(maxAge.Replace("max-age=", ""));
+                                Console.WriteLine($"Sleeping for {age} seconds");
+                                Thread.Sleep(age * 1000);
+                            }
+                        }
+
+                    } while (shouldPoll);
+
+                    var getResultResponse = axpOptimizerApi.GetOptimizationResultWithHttpInfo(optimizationId);
+                    getResultResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+                    getResultResponse.Data.Should().NotBeNull();
+                    break;
+                case HttpStatusCode.Created:
+                    break;
+            }
+        }
+    }
+}

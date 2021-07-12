704fbef29222c509a8f4429128b4dcaa0286e0ed
Kowsar Ahmed <76153409+kahmed00@users.noreply.github.com>
2021-07-12 16:24:08 -0400

    feat(examples): adding engines v3 calculation examples (#32)

---
+++ examples/FactSet.AnalyticsAPI.Engines.Example/Examples/PAEngineMultipleUnitExample.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading;
+
+using FactSet.AnalyticsAPI.Engines.Api;
+using FactSet.AnalyticsAPI.Engines.Client;
+using FactSet.AnalyticsAPI.Engines.Model;
+
+using FactSet.Protobuf.Stach.Extensions;
+
+namespace FactSet.AnalyticsAPI.Engines.Example.Examples
+{
+    public class PAEngineMultipleUnitExample
+    {
+        private static Configuration _engineApiConfiguration;
+        private const string BasePath = "https://api.factset.com";
+        private const string UserName = "<username-serial>";
+        private const string Password = "<apiKey>";
+        private const string PADocument = "PA_DOCUMENTS:DEFAULT";
+        private const string ComponentName = "Weights";
+        private const string ComponentCategory = "Weights / Exposures";
+        private const string BenchmarkSP50 = "BENCH:SP50";
+        private const string BenchmarkR1000 = "BENCH:R.1000";
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var calculationParameters = GetPaCalculationParameters();
+
+                var calculationApi = new PACalculationsApi(GetApiConfiguration());
+
+                var calculationResponse = calculationApi.PostAndCalculateWithHttpInfo(null, "max-stale=0", calculationParameters);
+
+                CalculationStatusRoot status = (CalculationStatusRoot)calculationResponse.Data;
+                var calculationId = status.Data.Calculationid;
+                Console.WriteLine("Calculation Id: " + calculationId);
+
+                ApiResponse<CalculationStatusRoot> getStatusResponse = null;
+
+                while (status.Data.Status == CalculationStatus.StatusEnum.Queued || status.Data.Status == CalculationStatus.StatusEnum.Executing)
+                {
+                    if (getStatusResponse != null)
+                    {
+                        if (getStatusResponse.Headers.ContainsKey("Cache-Control"))
+                        {
+                            var maxAge = getStatusResponse.Headers["Cache-Control"][0];
+                            if (string.IsNullOrWhiteSpace(maxAge))
+                            {
+                                Console.WriteLine("Sleeping for 2 seconds");
+                                // Sleep for at least 2 seconds.
+                                Thread.Sleep(2000);
+                            }
+                            else
+                            {
+                                var age = int.Parse(maxAge.Replace("max-age=", ""));
+                                Console.WriteLine($"Sleeping for {age} seconds");
+                                Thread.Sleep(age * 1000);
+                            }
+                        }
+                    }
+
+                    getStatusResponse = calculationApi.GetCalculationStatusByIdWithHttpInfo(calculationId);
+                    status = getStatusResponse.Data;
+                }
+                Console.WriteLine("Calculation Completed");
+
+                
+                foreach (var calculation in status.Data.Units)
+                {
+                    if (calculation.Value.Status == CalculationUnitStatus.StatusEnum.Success)
+                    {
+                        var resultResponse = calculationApi.GetCalculationUnitResultByIdWithHttpInfo(calculationId, calculation.Key);
+                        PrintResult(resultResponse.Data);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Calculation Unit Id : {calculation.Key} Failed!!!");
+                        Console.WriteLine($"Error message : {calculation.Value.Errors?.FirstOrDefault()?.Detail}");
+                    }
+                }
+
+                Console.ReadKey();
+            }
+            catch (ApiException e)
+            {
+                Console.WriteLine($"Status Code: {e.ErrorCode}");
+                Console.WriteLine($"Reason : {e.Message}");
+                Console.WriteLine(e.StackTrace);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        private static Configuration GetApiConfiguration()
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            if (_engineApiConfiguration != null)
+            {
+                return _engineApiConfiguration;
+            }
+
+            _engineApiConfiguration = new Configuration
+            {
+                BasePath = BasePath,
+                Username = UserName,
+                Password = Password
+            };
+
+            return _engineApiConfiguration;
+        }
+
+
+        private static PACalculationParametersRoot GetPaCalculationParameters()
+        {
+            var componentsApi = new ComponentsApi(GetApiConfiguration());
+
+            var componentsResponse = componentsApi.GetPAComponents(PADocument);
+
+            var paComponentId = componentsResponse.Data.FirstOrDefault(component => (component.Value.Name == ComponentName && component.Value.Category == ComponentCategory)).Key;
+            Console.WriteLine($"PA Component Id : {paComponentId}");
+
+            var paAccountIdentifier = new PAIdentifier(BenchmarkSP50);
+            var paAccounts = new List<PAIdentifier> { paAccountIdentifier };
+            var paBenchmarkIdentifier = new PAIdentifier(BenchmarkR1000);
+            var paBenchmarks = new List<PAIdentifier> { paBenchmarkIdentifier };
+
+            var paCalculation = new PACalculationParameters(paComponentId, paAccounts, paBenchmarks);
+
+            var calculationParameters = new PACalculationParametersRoot
+            {
+                Data = new Dictionary<string, PACalculationParameters> {
+                    { "1", paCalculation },
+                    { "2", paCalculation }
+                }
+            };
+
+            return calculationParameters;
+        }
+
+        private static void PrintResult(ObjectRoot result)
+        {
+            Console.WriteLine("Calculation Result");
+
+            // converting the data to Package object
+            var stachBuilder = StachExtensionFactory.GetRowOrganizedBuilder();
+            var stachExtension = stachBuilder.SetPackage(result.Data).Build();
+            // To convert package to 2D tables.
+            var tables = stachExtension.ConvertToTable();
+
+            Console.WriteLine(tables[0]);
+        }
+    }
+}

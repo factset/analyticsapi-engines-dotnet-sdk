886d6b9e729c4247853ffc84d1c3c58444ef3631
ckoulopoul <christine.koulopoulos@factset.com>
2021-09-10 11:38:35 -0400

    added env var back as api.factset.com with GH secret

--- tests/FactSet.AnalyticsAPI.Engines.Test/Api/CommonParameters.cs
+++ tests/FactSet.AnalyticsAPI.Engines.Test/Api/CommonParameters.cs
@@ -12,8 +12,7 @@ namespace FactSet.AnalyticsAPI.Engines.Test.Api
         public static readonly string Password = Environment.GetEnvironmentVariable("ANALYTICS_API_QAR_PASSWORD");
 
         // Add 'ANALYTICS_API_URL' environment variable with api url as value
-        public static readonly string BaseUrl = "https://api.factset.com";
-
+        public static readonly string BaseUrl = !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("ANALYTICS_API_URL")) ? Environment.GetEnvironmentVariable("ANALYTICS_API_URL") : "https://api.factset.com";
 
         public const string PADefaultDocument = "PA_DOCUMENTS:DEFAULT";
         public const string PADefaultComponentName = "Weights";

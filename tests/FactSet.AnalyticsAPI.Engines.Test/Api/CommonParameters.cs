0a2bb73fb970c7bff069410cd2d128af3cfbf805
ckoulopoul <christine.koulopoulos@factset.com>
2021-09-10 10:40:02 -0400

    testing baseurl as a hardcoded string

--- tests/FactSet.AnalyticsAPI.Engines.Test/Api/CommonParameters.cs
+++ tests/FactSet.AnalyticsAPI.Engines.Test/Api/CommonParameters.cs
@@ -12,7 +12,8 @@ namespace FactSet.AnalyticsAPI.Engines.Test.Api
         public static readonly string Password = Environment.GetEnvironmentVariable("ANALYTICS_API_QAR_PASSWORD");
 
         // Add 'ANALYTICS_API_URL' environment variable with api url as value
-        public static readonly string BaseUrl = !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("ANALYTICS_API_URL")) ? Environment.GetEnvironmentVariable("ANALYTICS_API_URL") : "https://api.factset.com";
+        public static readonly string BaseUrl = "test baseurl";
+
 
         public const string PADefaultDocument = "PA_DOCUMENTS:DEFAULT";
         public const string PADefaultComponentName = "Weights";

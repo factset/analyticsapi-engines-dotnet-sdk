c7e66ef1083b25d2913ac5010c9f041e11d00f46
snalkari <98750728+snalkari@users.noreply.github.com>
2022-11-04 15:36:42 +0530

    Test methods added for isarrayreturntype

--- tests/FactSet.AnalyticsAPI.Engines.Test/Api/QuantCalculationsApiTests.cs
+++ tests/FactSet.AnalyticsAPI.Engines.Test/Api/QuantCalculationsApiTests.cs
@@ -32,7 +32,8 @@ namespace FactSet.AnalyticsAPI.Engines.Test.Api
             var oneOfQuantFormulases = new List<OneOfQuantFormulas>()
             {
                 new OneOfQuantFormulas(new QuantScreeningExpression(expr : "P_PRICE", name : "Price (SCR)", source : QuantScreeningExpression.SourceEnum.ScreeningExpression)),
-                new OneOfQuantFormulas(new QuantFqlExpression(expr : "P_PRICE", name : "Price (SCR)",  source : QuantFqlExpression.SourceEnum.FqlExpression))
+                new OneOfQuantFormulas(new QuantFqlExpression(expr : "P_PRICE", name : "Price (SCR)",  source : QuantFqlExpression.SourceEnum.FqlExpression)),
+                new OneOfQuantFormulas(new QuantFqlExpression(expr : "P_PRICE(#DATE,#DATE-5D,#FREQ)", name : "Price", isArrayReturnType : false,  source : QuantFqlExpression.SourceEnum.FqlExpression))
             };
 
             var quantCalculation = new QuantCalculationParameters(universe: oneOfQuantUniverse, dates: oneOfQuantDates, formulas: oneOfQuantFormulases);
@@ -49,7 +50,7 @@ namespace FactSet.AnalyticsAPI.Engines.Test.Api
 
             return response;
         }
-
+        
         [TestMethod]
         public void EnginesApi_Quant_Get_Calculation_Success()
         {
@@ -116,12 +117,108 @@ namespace FactSet.AnalyticsAPI.Engines.Test.Api
                 Assert.IsInstanceOfType(resultResponse.Data, typeof(Stream), "Result response data should be of type Stream.");
             }
         }
-
+        
         [TestMethod]
         public void EnginesAPi_Quant_GetAll_Calculations_Success()
         {
             var calculationsResponse = calculationsApi.GetAllCalculationsWithHttpInfoAsync(pageNumber);
             Assert.IsTrue(calculationsResponse.Result.StatusCode == HttpStatusCode.OK, "Result response status code should be 200 - OK.");
+        }
+
+        private ApiResponse<object> EnginesApi_Quant_IsArrayReturnType_RunCalculation()
+        {
+            var oneOfQuantUniverse = new OneOfQuantUniverse(new QuantIdentifierUniverse(
+                QuantIdentifierUniverse.UniverseTypeEnum.Equity,
+                new List<string> {  "03748R74", "S8112735" },
+                QuantIdentifierUniverse.SourceEnum.IdentifierUniverse));
+            var oneOfQuantDates = new OneOfQuantDates(new QuantFdsDate("0", "-5D", QuantFdsDate.SourceEnum.FdsDate, "D", "FIVEDAY"));
+            var oneOfQuantFormulases = new List<OneOfQuantFormulas>()
+            {
+                new OneOfQuantFormulas(new QuantScreeningExpression(expr : "P_PRICE", name : "Price (SCR)", source : QuantScreeningExpression.SourceEnum.ScreeningExpression)),
+                new OneOfQuantFormulas(new QuantFqlExpression(expr : "P_PRICE", name : "Price (SCR)",  source : QuantFqlExpression.SourceEnum.FqlExpression)),
+                new OneOfQuantFormulas(new QuantFqlExpression(expr : "P_PRICE(#DATE,#DATE-5D,#FREQ)", name : "Price", isArrayReturnType : true,  source : QuantFqlExpression.SourceEnum.FqlExpression))
+            };
+
+            var quantCalculation = new QuantCalculationParameters(universe: oneOfQuantUniverse, dates: oneOfQuantDates, formulas: oneOfQuantFormulases);
+
+            var quantCalculationsMeta = new QuantCalculationMeta(format: QuantCalculationMeta.FormatEnum.Feather);
+
+            var calculationParameters = new QuantCalculationParametersRoot
+            {
+                Data = new Dictionary<string, QuantCalculationParameters> { { "1", quantCalculation } },
+                Meta = quantCalculationsMeta
+            };
+
+            var response = calculationsApi.PostAndCalculateWithHttpInfo("max-stale=0", calculationParameters);
+
+            return response;
+        }
+        
+        [TestMethod]
+        public void EnginesApi_Quant_IsArrayReturnType_Get_Calculation_Success()
+        {
+            var calculationResponse = EnginesApi_Quant_IsArrayReturnType_RunCalculation();
+
+            Assert.IsTrue(calculationResponse.StatusCode == HttpStatusCode.Accepted, "Create response status code should be 202 - Accepted.");
+
+            CalculationStatusRoot calculationStatus = (CalculationStatusRoot)calculationResponse.Data;
+
+            var calculationId = calculationStatus.Data.Calculationid;
+
+            Assert.IsTrue(!string.IsNullOrWhiteSpace(calculationId), "Create response calculation id should be present.");
+
+            ApiResponse<CalculationStatusRoot> getStatusResponse = null;
+
+            while (calculationStatus.Data.Status == CalculationStatus.StatusEnum.Queued || calculationStatus.Data.Status == CalculationStatus.StatusEnum.Executing)
+            {
+                if (getStatusResponse != null)
+                {
+                    Assert.IsTrue(getStatusResponse.Data != null, "Response Data should not be null.");
+                    Assert.IsTrue(getStatusResponse.Data.Data.Status == CalculationStatus.StatusEnum.Executing || getStatusResponse.Data.Data.Status == CalculationStatus.StatusEnum.Queued, "Response Data should have batch status as processing or queued.");
+
+                    if (getStatusResponse.Headers.ContainsKey("Cache-Control"))
+                    {
+                        var maxAge = getStatusResponse.Headers["Cache-Control"][0];
+                        if (string.IsNullOrWhiteSpace(maxAge))
+                        {
+                            Console.WriteLine("Sleeping for 2 seconds");
+                            // Sleep for at least 2 seconds.
+                            Thread.Sleep(2000);
+                        }
+                        else
+                        {
+                            var age = int.Parse(maxAge.Replace("max-age=", ""));
+                            Console.WriteLine($"Sleeping for {age} seconds");
+                            Thread.Sleep(age * 1000);
+                        }
+                    }
+                }
+
+                getStatusResponse = calculationsApi.GetCalculationStatusByIdWithHttpInfo(calculationId);
+                calculationStatus = getStatusResponse.Data;
+            }
+
+            Assert.IsTrue(calculationStatus.Data.Status == CalculationStatus.StatusEnum.Completed, "Response Data should have calculation status as completed.");
+            Assert.IsTrue(calculationStatus.Data.Units.Values.All(p => p.Status == CalculationUnitStatus.StatusEnum.Success), "Response Data should have all calculations status as completed.");
+
+            Assert.IsTrue(calculationStatus.Data.Units.Values.All(p => p.Result != null), "Response Data should have all Calculation results.");
+
+            foreach (var calculation in calculationStatus.Data.Units)
+            {
+                var resultResponse = calculationsApi.GetCalculationUnitResultByIdWithHttpInfo(calculationId, calculation.Key);
+
+                Assert.IsTrue(resultResponse.StatusCode == HttpStatusCode.OK, "Result response status code should be 200 - OK.");
+                Assert.IsTrue(resultResponse.Data != null, "Result response data should not be null.");
+
+                Assert.IsInstanceOfType(resultResponse.Data, typeof(Stream), "Result response data should be of type Stream.");
+
+                var infoResponse = calculationsApi.GetCalculationUnitInfoByIdWithHttpInfo(calculationId, calculation.Key);
+
+                Assert.IsTrue(resultResponse.StatusCode == HttpStatusCode.OK, "Result response status code should be 200 - OK.");
+                Assert.IsTrue(resultResponse.Data != null, "Result response data should not be null.");
+
+                Assert.IsInstanceOfType(resultResponse.Data, typeof(Stream), "Result response data should be of type Stream.");
+            }
         }
     }
 }
